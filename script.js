a95184f9102fe4460ea1b44c9828febea0a92efe
Akash Shokeen <117618540+AkashShokeen@users.noreply.github.com>
2023-08-13 15:31:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-const team = player;
+const team = players;
 const team1 = ["John", "Bob", "Alice", "Poppy"];
 
 const cap1 = {
